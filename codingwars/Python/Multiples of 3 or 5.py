faebaf891af67d98de53b1437ac8049c08abfa5e
kmb21 <113995857+kmb21@users.noreply.github.com>
2023-02-02 12:56:02 -0500

    Created multiples of 3 or 5

---
+++ codingwars/Python/Multiples of 3 or 5.py
@@ -0,0 +1,11 @@
+def solution(number):
+    sum = 0
+    if number < 0:
+        return 0
+    else:
+        for num in range(number):
+            if num%3==0 and num%5 == 0:
+                sum += num
+            elif num%3 == 0 or num%5 ==0:
+                sum += num
+    return sum
