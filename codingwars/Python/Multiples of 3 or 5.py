26ef7059120cc279ce1d63e298a44901657c8e50
kmb21 <113995857+kmb21@users.noreply.github.com>
2023-02-02 12:58:24 -0500

    update code of multiples

--- codingwars/Python/Multiples of 3 or 5.py
+++ codingwars/Python/Multiples of 3 or 5.py
@@ -4,8 +4,7 @@ def solution(number):
         return 0
     else:
         for num in range(number):
-            if num%3==0 and num%5 == 0:
-                sum += num
-            elif num%3 == 0 or num%5 ==0:
+            if num%3 == 0 or num%5 ==0:
                 sum += num
     return sum
+  
